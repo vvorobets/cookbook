e4174e5ce216da4675b39cb09ef3a1f5f543a8be
vvorobets <vvorobets@gmail.com>
2018-07-25 10:14:16 +0300

    Update view for empty recipe list

--- src/components/EmptyContainer.js
+++ src/components/EmptyContainer.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { Grid, Segment, Button, Image, Icon, Message } from "semantic-ui-react";
+import { Grid, Header, Button, Image } from "semantic-ui-react";
 
 const EmptyContainer = () => (
-  <Grid.Row centered>
-    <Segment size="huge" inverted color="red" textAlign="center">
-      You don't have any recipes!
-    </Segment>
-    <Message>
-      <p>You don't have any recipes!</p>
-    </Message>
-    <Image
-      size="large"
-      src="http://www.siouxfallscompletefitness.com/wp-content/uploads/2013/01/empty_plate.jpg"
-    />
-    <Button positive fluid size="huge">
-      <Icon name="plus" />
-      Add immediately!
-    </Button>
-  </Grid.Row>
+  <React.Fragment>
+    <Grid.Row centered>
+      <Header as='h3' block color="red" textAlign="center" content="You don't have any recipes!" />
+    </Grid.Row >
+    <Grid.Row centered>
+      <Image
+        size="large"
+        src="http://www.siouxfallscompletefitness.com/wp-content/uploads/2013/01/empty_plate.jpg"
+      />
+    </Grid.Row >
+    <Grid.Row centered>
+      <Button positive icon="plus" content="Add immediately!" size="huge" />
+    </Grid.Row>
+  </React.Fragment>
 );
 
 export default EmptyContainer;
